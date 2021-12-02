9180e1425c2b100b82b06da72ee72b4140fe9ad3
alexandre-zatti <zatti.alexandre61@gmail.com>
2021-12-02 15:48:13 -0300

    commit save

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,17 +1,23 @@
 import React, {useEffect, useState} from "react";
-import {FlatList, Image, StyleSheet, TouchableOpacity, View} from "react-native";
+import {FlatList, Image, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {FontAwesome} from '@expo/vector-icons';
 import Modal from "react-native-modal";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Gallery = ({ navigation } : any) : JSX.Element => {
     const [fetchUrl] = useState(`https://api.nasa.gov/mars-photos/api/v1/rovers/${navigation.getParam('name')}/photos?api_key=gWTCE5U2htOMEOdnKYFAFQKaYHCMLaBw73dkVFWJ&earth_date=${navigation.getParam('pickedDate')}${navigation.getParam('pickedData').value !== "" ? '&camera='+navigation.getParam('pickedData').value : ''}`)
     const [galleryData, setGalleryData] = useState()
     const [openModal, setOpenModal] = useState(false)
     const [modalImage, setModalImage] = useState()
+    const [emptyGallery, setEmptyGallery] = useState(true)
 
     useEffect(() => {
         fetch(fetchUrl).then((response) => {
             response.json().then((json) => {
-                setGalleryData(json['photos'])
+                if(json['photos'].length){
+                    setEmptyGallery(false)
+                    setGalleryData(json['photos'])
+                }
             })
         })
     },[])
@@ -27,15 +33,22 @@ const Gallery = ({ navigation } : any) : JSX.Element => {
                     <Image style={styles.modalImage} source={{uri: modalImage}}/>
                 </View>
             </Modal>
-            <FlatList
-                contentContainerStyle={styles.list}
-                numColumns={2}
-                data={galleryData}
-                renderItem={({item}) => (
-                <TouchableOpacity onPress={() => {setOpenModal(true); setModalImage(item.img_src)}}>
-                    <Image style={styles.image} source={{uri: item.img_src}}/>
-                </TouchableOpacity>
-            )}/>
+            {emptyGallery ? <View><Text>Nenhuma foto encontrada!</Text></View> :
+                <FlatList
+                    contentContainerStyle={styles.list}
+                    numColumns={2}
+                    data={galleryData}
+                    renderItem={({item}) => (
+                        <TouchableOpacity onPress={() => {setOpenModal(true); setModalImage(item.img_src)}}>
+                            <Image style={styles.image} source={{uri: item.img_src}}/>
+                            <FontAwesome style={styles.favorite} name={'star'} size={36} onPress={ async () => {
+                                await AsyncStorage.setItem("test", item)
+                                let value = await AsyncStorage.getItem("test")
+                                console.log(value)
+                            }}/>
+                        </TouchableOpacity>
+                    )}/>
+            }
         </View>
     )
 }
@@ -53,6 +66,12 @@ const styles = StyleSheet.create({
         margin: 5,
         borderRadius: 10
     },
+    favorite: {
+        position: "absolute",
+        right:10,
+        bottom: 10,
+        color: '#fff',
+    },
     list: {
         alignItems: "center",
         justifyContent: "flex-start",
@@ -65,14 +84,14 @@ const styles = StyleSheet.create({
     },
     modalView: {
         height: 500,
-        width: 350,
+        width: 375,
         backgroundColor: '#fff',
         borderRadius:20,
         alignItems: "center",
         justifyContent: "center"
     },
     modalImage: {
-        width: 350,
+        width: 375,
         height: 500,
         borderRadius:20,
     }
